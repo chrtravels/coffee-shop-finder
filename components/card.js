3cf1e6da3788d26287c5e5cd3165bcf832d2b8b2
Chris Reynoso <chrtravels@gmail.com>
2022-07-31 14:54:56 -0700

    added styling & glass morphism

--- components/card.js
+++ components/card.js
@@ -1,15 +1,22 @@
 import { loadGetInitialProps } from 'next/dist/shared/lib/utils';
 import Link from 'next/link';
 import Image from 'next/image';
+import cls from 'classnames';
 import styles from './card.module.css'
 
 const card = (props) => {
   return (
     <Link href={props.href}>
-      <a>
-        <div>
-          <h2>{props.name}</h2>
-          <Image src={props.imgUrl} width={260} height={160} />
+      <a className={styles.cardLink}>
+        <div className={cls("glass", "glass:hover", styles.container)}>
+          <div>
+            <div className={styles.cardHeaderWrapper}>
+              <h2 className={styles.cardHeader}>{props.name}</h2>
+            </div>
+            <div className={styles.cardImageWrapper}>
+              <Image className={styles.cardImage} src={props.imgUrl} width={260} height={160} />
+            </div>
+          </div>
         </div>
       </a>
     </Link>
