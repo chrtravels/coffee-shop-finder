61bf6dc9a16b986ccda78c6ba196256e32f9ed86
Chris Reynoso <chrtravels@gmail.com>
2022-07-31 21:09:01 -0700

    setup dynamic paths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,8 +1,37 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-const CoffeeStore = () => {
+import coffeeStoresData from '../../data/coffee-stores.json';
+
+export function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find(coffeeStore => {
+        return coffeeStore.id.toString() === params.id // dynamic id
+      }),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: "0" } },
+      { params: { id: "1" } },
+    ],
+    // fallback true is very helpful if you have a lot of pages
+    fallback: true,
+  };
+}
+
+const CoffeeStore = (props) => {
   const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+
   return (
       <div>Coffee Store Page {router.query.id}
         <Link href='/'>
@@ -11,6 +40,8 @@ const CoffeeStore = () => {
         <Link href='/coffee-store/dynamic'>
           <a>Go to dynamic page</a>
         </Link>
+        <p>{props.coffeeStore.address}</p>
+        <p>{props.coffeeStore.name}</p>
       </div>
   );
 };
