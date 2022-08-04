c68ca0defd4942e9e263110fbeb38915b7dbeb9e
Chris Reynoso <chrtravels@gmail.com>
2022-08-04 12:56:33 -0700

    update coffee store library data

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -48,7 +48,7 @@ const CoffeeStore = (props) => {
     return <div>Loading...</div>
   }
 
-  const { address, name, imgUrl, neighbourhood } = props.coffeeStore;
+  const { location, name, imgUrl } = props.coffeeStore;
 
   const handleUpvoteButton = () => {
     console.log("handle Upvote");
@@ -76,11 +76,11 @@ const CoffeeStore = (props) => {
           <div className={cls("glass", styles.col2)}>
             <div className={styles.iconWrapper}>
               <Image src="/static/icons/places.svg" width="24" height="24" />
-              <p className={styles.text}>{address}</p>
+              <p className={styles.text}>{location.address}</p>
             </div>
             <div className={styles.iconWrapper}>
               <Image src="/static/icons/nearMe.svg" width="24" height="24" />
-              <p className={styles.text}>{neighbourhood}</p>
+              <p className={styles.text}>{location.neighborhood}</p>
             </div>
             <div className={styles.iconWrapper}>
               <Image src="/static/icons/star.svg" width="24" height="24" />
